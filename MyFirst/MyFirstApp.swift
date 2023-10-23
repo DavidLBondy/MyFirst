168cd8829e3920a6c73b68e238db41b0c787bdc0
David Bondy <david.l.bondy@gmail.com>
2023-10-22 20:25:57 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by David Bondy on 2023-10-22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
